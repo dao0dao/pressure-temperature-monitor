0b25e699ce48ec9dfcfe161d422ea348c2200fac
Demid Greshnikov <demid.westhill@gmail.com>
2026-07-25 18:04:45 +0200

    initial; Assembled piece by piece late at night

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './layout/nav/nav.component';
+import { AuthService } from './core/auth/auth.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
-export class AppComponent {
-  title = 'pressure-temperature-monitor';
+export class App {
+  authService = inject(AuthService);
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth/auth.guard';
+import { logInGuard } from './core/auth/log-in.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    canActivate: [logInGuard],
+    loadComponent: () => import('./components/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'monitor',
+    canActivate: [authGuard],
+    loadComponent: () => import('./components/monitor/monitor.component').then((m) => m.MonitorComponent),
+  },
+  {
+    path: 'add-data',
+    canActivate: [authGuard],
+    loadComponent: () => import('./components/add-data/add-data.component').then((m) => m.AddDataComponent),
+  },
+  { path: '**', redirectTo: '' },
+];
