6f1bd8756500e026d695cae68021cdb6ba5982da
Demid Greshnikov <demid.westhill@gmail.com>
2026-07-28 09:55:12 +0200

    add translation; add pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, inject, provideExperimentalZonelessChangeDetection, APP_INITIALIZER } from '@angular/core';
+import { ApplicationConfig, inject, provideExperimentalZonelessChangeDetection, APP_INITIALIZER, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
@@ -7,6 +7,7 @@ import { routes } from './app.routes';
 import { TranslationService } from './core/i18n/translate.service';
 import { DEFAULT_LOCALE } from './shared/i18n/models/locale.model';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 function initializeTranslations(): () => Promise<void> {
   const translationService = inject(TranslationService);
@@ -26,6 +27,9 @@ export const appConfig: ApplicationConfig = {
       provide: APP_INITIALIZER,
       useFactory: initializeTranslations,
       multi: true
-    }
+    }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ],
 };
